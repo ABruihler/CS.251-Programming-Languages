8aa4485960bd275e6b0ecb955cbceb90104069d4
Anders Bruihler <6530276+ABruihler@users.noreply.github.com>
2019-01-23 20:47:24 -0600

    sieve implement nth

--- Sieve_of_Eratosthenes/sieve.rkt
+++ Sieve_of_Eratosthenes/sieve.rkt
@@ -22,12 +22,29 @@
       ((not lazy-list) ())
       (else (append (list (car lazy-list)) (first-n ((cdr lazy-list)) (- n 1)))))))
 
+;; Returns the n-th value in the given lazy list.  If invalid n, then returns #f
+(define nth
+  (lambda (lazy-list n)
+    (cond
+      ((< n 1) #f)
+      ((not lazy-list) #f)
+      ((equal? n 1) (car lazy-list))
+      (else (nth ((cdr lazy-list)) (- n 1))))))
+
 
 ;; Tests below
 
-;;(require racket/trace)
-;;(trace first-n)
+(require racket/trace)
+;;(trace nth)
 
-(first-n (seq 1 10) 0) ;; () (doesn't even need to be handled but it turns out I do
+(first-n (seq 1 10) 0) ;; () (doesn't even need to be handled but it turns out I do)
+(first-n (seq 1 10) 1) ;; (1)
 (first-n (seq 1 10) 5) ;; (1 2 3 4 5)
 (first-n (seq 1 10) 11) ;; (1 2 3 4 5 6 7 8 9 10)
+
+(newline)
+(nth (seq 1 10) 0) ;; #f
+(nth (seq 1 10) 1) ;; 1
+(nth (seq 1 10) 5) ;; 5
+(nth (seq 1 10) 10) ;; 10
+(nth (seq 1 10) 11) ;; #f
